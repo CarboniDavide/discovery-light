98d751aba9632528c7c61db0a0427f388b5654e2
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-03 01:52:21 +0100

    prepare new base project strucuture

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DiscoveryLight.UI.Forms.Main;
 
 namespace DiscoveryLight
 {
@@ -16,7 +17,7 @@ namespace DiscoveryLight
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new _Main());
         }
     }
 }
