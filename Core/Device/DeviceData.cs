aaf6085f42948ca2627d2a7a2a7412adc1460d3b
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-08 00:14:39 +0100

    change namespace

--- Core/Device/DeviceData.cs
+++ Core/Device/DeviceData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Management;
 
-namespace DiscoveryLight.Core.Device.Performance
+namespace DiscoveryLight.Core.Device.Data
 {
     public class PC
     {
