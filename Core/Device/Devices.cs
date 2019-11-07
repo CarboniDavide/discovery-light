e843ed7b65d512865285ed67f26e7b8ebdceebe8
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-08 00:15:07 +0100

    add new class to manage device collection

---
+++ Core/Device/Devices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscoveryLight.Core.Device.Data;
+
+namespace DiscoveryLight.Core.Device
+{
+    class Devices
+    {
+        public AUDIO Audio;
+        public CPU Cpu;
+        public DISK Disk;
+        public BIOS Bios;
+        public SCORE Score;
+        public RAM Ram;
+        public ETHERNET Ethernet;
+        public PC Pc;
+        public VIDEO Video;
+    }
+}
