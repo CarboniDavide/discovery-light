d4b4173da00b06201b1ce9e91e2a58a1b3fc9a2f
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-08 23:38:20 +0100

    generate base layout structure

--- UI/Forms/Main/_Main.Designer.cs
+++ UI/Forms/Main/_Main.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace DiscoveryLight.UI.Forms.Main
+﻿using DiscoveryLight.UI.Panels.UserSettings;
+using DiscoveryLight.UI.Panels.Devices;
+
+namespace DiscoveryLight.UI.Forms.Main
 {
     partial class _Main
     {
@@ -28,12 +31,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(_Main));
+            this.DevicePanel = new DiscoveryLight.UI.Panels.Devices._Base();
+            this.FooterBar = new DiscoveryLight.UI.Forms.Main._Footer();
+            this.NavigationBar = new DiscoveryLight.UI.Forms.Main._Navigation();
+            this.SuspendLayout();
+            // 
+            // DevicePanel
+            // 
+            this.DevicePanel.AutoSize = true;
+            this.DevicePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DevicePanel.Location = new System.Drawing.Point(40, 0);
+            this.DevicePanel.Name = "DevicePanel";
+            this.DevicePanel.Size = new System.Drawing.Size(632, 360);
+            this.DevicePanel.TabIndex = 3;
+            // 
+            // FooterBar
+            // 
+            this.FooterBar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.FooterBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.FooterBar.Location = new System.Drawing.Point(40, 360);
+            this.FooterBar.Name = "FooterBar";
+            this.FooterBar.Size = new System.Drawing.Size(632, 40);
+            this.FooterBar.TabIndex = 2;
+            // 
+            // NavigationBar
+            // 
+            this.NavigationBar.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.NavigationBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.NavigationBar.Location = new System.Drawing.Point(0, 0);
+            this.NavigationBar.Name = "NavigationBar";
+            this.NavigationBar.Size = new System.Drawing.Size(40, 400);
+            this.NavigationBar.TabIndex = 1;
+            // 
+            // _Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "_Main";
+            this.ClientSize = new System.Drawing.Size(672, 400);
+            this.Controls.Add(this.DevicePanel);
+            this.Controls.Add(this.FooterBar);
+            this.Controls.Add(this.NavigationBar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "_Main";
+            this.Text = "Discovery";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        public _Base DevicePanel;
+        public _Footer FooterBar;
+        public _Navigation NavigationBar;
+        public _UserSettings UserSettings;
     }
 }
