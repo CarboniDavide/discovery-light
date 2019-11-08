c74d19f5fca292bbac7565afb84a29477e457e6b
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-08 23:37:01 +0100

    change bkg color

--- UI/Forms/Main/_Navigation.Designer.cs
+++ UI/Forms/Main/_Navigation.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // _Navigation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Name = "_Navigation";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
