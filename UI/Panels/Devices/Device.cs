3f6a028ae258efd49fdc72907f587acd53b9129c
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-09 17:08:22 +0100

    add new device interface

---
+++ UI/Panels/Devices/Device.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoveryLight.UI.Panels.Devices
+{
+    public interface Device
+    {
+    }
+}
