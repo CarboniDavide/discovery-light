ad16385f89c7ed55d336d85ee06460332af72d1a
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-10 17:28:52 +0100

    add performance class

---
+++ Core/Device/PerformanceBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoveryLight.Core.Device.Performance
+{
+    class Performance
+    {
+        private String name;
+        private DevicePerformance properties;
+
+        public string Name { get => name; set => name = value; }
+        public DevicePerformance Properties
+    {
+            get { return properties; }
+            set
+            {
+                properties = value;
+                this.Name = this.properties.GetType().Name;
+            }
+        }
+
+        public Performance(DevicePerformance Performance)
+        {
+            this.Properties = Performance;
+        }
+    }
+}
