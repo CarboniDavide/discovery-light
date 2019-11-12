991221bf6dcc5f86750753a2e80cccd612a45f13
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-12 19:09:49 +0100

    Implements new device controls

---
+++ UI/Controls/DeviceDataControls/DeviceDataControl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscoveryLight.Core.Device.Data;
+
+namespace DiscoveryLight.UI.Controls.DeviceDataControls
+{
+    public abstract class AbstractDeviceDataControl : System.Windows.Forms.UserControl
+    {
+        public abstract void InitData(DeviceData Device);
+        public abstract void ShowData();
+    }
+    public class DeviceDataControl: AbstractDeviceDataControl
+    {
+        private DeviceData currentDevice;
+        private DeviceData._Block currentSubDevice;
+        private Type deviceType;
+        private String deviceName;
+
+        public DeviceData CurrentDevice
+        {
+            get { return currentDevice; }
+            set
+            {
+                currentDevice = value;
+                deviceType = currentDevice.GetType();
+                deviceName = currentDevice.ToString();
+            }
+        }
+        public DeviceData._Block CurrentSubDevice { get => currentSubDevice; set => currentSubDevice = value; }
+        public Type DeviceType { get => deviceType; set => deviceType = value; }
+        public string DeviceName { get => deviceName; set => deviceName = value; }
+
+        public override void InitData(DeviceData Device)
+        {
+            CurrentDevice = Device;
+            CurrentSubDevice = (CurrentDevice.Blocks.Count != 0) ? CurrentDevice.Blocks.First() : new DeviceData._Block();
+        }
+        public override void ShowData() { }
+
+        public DeviceDataControl(DeviceData Device)
+        {
+            InitData(Device);
+        }
+    }
+}
---
+++ UI/Controls/DevicePerformanceControls/DevicePerformanceControl.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DiscoveryLight.Core.Device.Performance;
+
+namespace DiscoveryLight.UI.Controls.DevicePerformanceControls
+{
+    public abstract class AbstractDevicePerformanceControl : System.Windows.Forms.UserControl
+    {
+        public abstract void InitPerformace(DevicePerformance Device);
+        public abstract void ShowPerformance();
+        public abstract void RunPerformance();
+        public abstract void StopPerformance();
+    }
+
+    class DevicePerformanceControl: AbstractDevicePerformanceControl
+    {
+        private DevicePerformance currentPerformance;
+        private int currentSubDevice;
+        private Type deviceType;
+        private String deviceName;
+
+        private CancellationTokenSource tokenSource;
+        private CancellationToken token;
+        private TimeSpan period;
+
+        public DevicePerformance CurrentPerformance
+        {
+            get { return currentPerformance; }
+            set
+            {
+                currentPerformance = value;
+                deviceType = currentPerformance.GetType();
+                deviceName = deviceType.ToString();
+                currentSubDevice = 0;
+            }
+        }
+        public int CurrentSubDevice { get => currentSubDevice; set => currentSubDevice = value; }
+        public CancellationTokenSource TokenSource { get => tokenSource; set => tokenSource = value; }
+        public CancellationToken Token { get => token; set => token = value; }
+        public TimeSpan Period { get => period; set => period = value; }
+
+        public override void InitPerformace(DevicePerformance Performance)
+        {
+            CurrentPerformance = Performance;
+            tokenSource = new CancellationTokenSource();
+            token = TokenSource.Token;
+            period = TimeSpan.FromMilliseconds(500);
+        }
+        public override void ShowPerformance() { }
+
+        public override async void RunPerformance()
+        {
+            StopPerformance();
+            await Run();
+        }
+
+        public async Task Run()
+        {
+            while (!this.token.IsCancellationRequested)
+            {
+                await Task.Delay(period, token);
+
+                if (!token.IsCancellationRequested)
+                {
+                    await Task.Run(() => currentPerformance.GetPerformance());
+                    ShowPerformance();
+                }
+            }
+        }
+
+        public override void StopPerformance()
+        {
+            if (token != null)
+                tokenSource.Cancel();
+        }
+
+        public DevicePerformanceControl(DevicePerformance Device)
+        {
+            InitPerformace(Device);
+        }
+    }
+}
