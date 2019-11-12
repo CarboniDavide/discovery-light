cf9a52a8735fdde54c40d5eca1ef93ddf8b869ae
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-13 00:38:24 +0100

    refactoring cpu

---
+++ UI/DeviceControls/DevicePerformanceControls/_CpuDevicePerformanceControl.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace DiscoveryLight.UI.DeviceControls.DevicePerformanceControls
+{
+    partial class _CpuDevicePerformanceControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chartCpuUsage = new WinformComponents.ChartCircle();
+            this.lbl_CpuUsage_Value = new System.Windows.Forms.Label();
+            this.lbl_CpuUsage = new System.Windows.Forms.Label();
+            this.pnl_Threads = new System.Windows.Forms.Panel();
+            this.pic_Divisor_002 = new System.Windows.Forms.PictureBox();
+            this._SystemDevicePerformanceControl1 = new DiscoveryLight.UI.DeviceControls.DevicePerformanceControls._SystemDevicePerformanceControl();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Divisor_002)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartCpuUsage
+            // 
+            this.chartCpuUsage.Activated = true;
+            this.chartCpuUsage.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.chartCpuUsage.CircleBackColor = System.Drawing.Color.LightGray;
+            this.chartCpuUsage.CircleFillColor = System.Drawing.Color.YellowGreen;
+            this.chartCpuUsage.FillSize = 25;
+            this.chartCpuUsage.Location = new System.Drawing.Point(21, 5);
+            this.chartCpuUsage.Name = "chartCpuUsage";
+            this.chartCpuUsage.Size = new System.Drawing.Size(90, 90);
+            this.chartCpuUsage.TabIndex = 64;
+            this.chartCpuUsage.TextVisible = true;
+            this.chartCpuUsage.Thickness = 10;
+            // 
+            // lbl_CpuUsage_Value
+            // 
+            this.lbl_CpuUsage_Value.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lbl_CpuUsage_Value.AutoSize = true;
+            this.lbl_CpuUsage_Value.Location = new System.Drawing.Point(195, 81);
+            this.lbl_CpuUsage_Value.Name = "lbl_CpuUsage_Value";
+            this.lbl_CpuUsage_Value.Size = new System.Drawing.Size(24, 13);
+            this.lbl_CpuUsage_Value.TabIndex = 78;
+            this.lbl_CpuUsage_Value.Text = "n/a";
+            // 
+            // lbl_CpuUsage
+            // 
+            this.lbl_CpuUsage.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lbl_CpuUsage.AutoSize = true;
+            this.lbl_CpuUsage.Location = new System.Drawing.Point(140, 81);
+            this.lbl_CpuUsage.Name = "lbl_CpuUsage";
+            this.lbl_CpuUsage.Size = new System.Drawing.Size(41, 13);
+            this.lbl_CpuUsage.TabIndex = 74;
+            this.lbl_CpuUsage.Text = "Utilisée";
+            // 
+            // pnl_Threads
+            // 
+            this.pnl_Threads.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pnl_Threads.Location = new System.Drawing.Point(7, 117);
+            this.pnl_Threads.Name = "pnl_Threads";
+            this.pnl_Threads.Size = new System.Drawing.Size(267, 160);
+            this.pnl_Threads.TabIndex = 79;
+            // 
+            // pic_Divisor_002
+            // 
+            this.pic_Divisor_002.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pic_Divisor_002.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pic_Divisor_002.Location = new System.Drawing.Point(135, 31);
+            this.pic_Divisor_002.Name = "pic_Divisor_002";
+            this.pic_Divisor_002.Size = new System.Drawing.Size(5, 63);
+            this.pic_Divisor_002.TabIndex = 75;
+            this.pic_Divisor_002.TabStop = false;
+            // 
+            // _SystemDevicePerformanceControl1
+            // 
+            this._SystemDevicePerformanceControl1.BackColor = System.Drawing.Color.Transparent;
+            this._SystemDevicePerformanceControl1.CurrentPerformance = null;
+            this._SystemDevicePerformanceControl1.CurrentSubDevice = 0;
+            this._SystemDevicePerformanceControl1.Location = new System.Drawing.Point(141, 27);
+            this._SystemDevicePerformanceControl1.Name = "_SystemDevicePerformanceControl1";
+            this._SystemDevicePerformanceControl1.Period = System.TimeSpan.Parse("00:00:00");
+            this._SystemDevicePerformanceControl1.Size = new System.Drawing.Size(140, 48);
+            this._SystemDevicePerformanceControl1.TabIndex = 80;
+            this._SystemDevicePerformanceControl1.TokenSource = null;
+            // 
+            // _CpuDevicePerformanceControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._SystemDevicePerformanceControl1);
+            this.Controls.Add(this.pnl_Threads);
+            this.Controls.Add(this.lbl_CpuUsage_Value);
+            this.Controls.Add(this.pic_Divisor_002);
+            this.Controls.Add(this.lbl_CpuUsage);
+            this.Controls.Add(this.chartCpuUsage);
+            this.Name = "_CpuDevicePerformanceControl";
+            this.Size = new System.Drawing.Size(281, 286);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Divisor_002)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private WinformComponents.ChartCircle chartCpuUsage;
+        private System.Windows.Forms.PictureBox pic_Divisor_002;
+        private System.Windows.Forms.Label lbl_CpuUsage_Value;
+        private System.Windows.Forms.Label lbl_CpuUsage;
+        private System.Windows.Forms.Panel pnl_Threads;
+        private _SystemDevicePerformanceControl _SystemDevicePerformanceControl1;
+    }
+}
---
+++ UI/DeviceControls/DevicePerformanceControls/_CpuDevicePerformanceControl.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiscoveryLight.Core.Device.Performance;
+using DiscoveryLight.Core.Device.Data;
+
+namespace DiscoveryLight.UI.DeviceControls.DevicePerformanceControls
+{
+    public partial class _CpuDevicePerformanceControl : DevicePerformanceControl
+    {
+        public _CpuDevicePerformanceControl(): base()
+        {
+            InitializeComponent();
+            if (Program.Performances == null) return;
+            InitPerformace(Program.Performances.Where(d => d.Properties.GetType() == typeof(PERFORM_CPU)).First().Properties);
+        }
+
+        public override void ShowPerformance()
+        {
+            base.ShowPerformance();
+            var CurrentPerformanceCPU = (PERFORM_CPU)this.CurrentPerformance;
+            lbl_CpuUsage_Value.Text = CurrentPerformanceCPU.Cpu.Where(d => d.Name == CurrentPerformanceCPU.SelectedCpu + "," + "_Total").First().Frequency + " Mhz";
+            chartCpuUsage.FillSize = Convert.ToInt16(CurrentPerformanceCPU.Cpu.Where(d => d.Name == CurrentPerformanceCPU.SelectedCpu + "," + "_Total").First().DPCRate);
+            int i = 0;
+            foreach (WinformComponents.ChartBar ctrl in pnl_Threads.Controls.Find(typeof(WinformComponents.ChartBar).ToString(), false))
+            {
+                ctrl.BarFillSize = Convert.ToInt16(CurrentPerformanceCPU.Cpu.Where(d => d.Name == CurrentPerformanceCPU.SelectedCpu + "," + i).First().DPCRate);
+                i++;
+            }
+        }
+
+        public void GraphComponents_Add()
+        {
+            Clear();
+            int BarPosition;
+            int Step;
+            int BarSize;
+            int TextSize;
+
+            CPU cpu= new CPU();
+            cpu.GetDriveInfo();
+            var CurrentSubDevice = (CPU.Block)cpu.Blocks.Where(b => b.DeviceID.Equals(this.CurrentSubDevice.ToString())).FirstOrDefault();
+            // Création et insertion des élements grapique
+
+            // Calcul de l'épessuer entré les graphique à barre
+            Step = pnl_Threads.Height / (Convert.ToInt16(CurrentSubDevice.N_Thread));
+
+            // Calcul de l'hauteur du caques graphique à barre
+            BarSize = Convert.ToInt16(pnl_Threads.Height / (Convert.ToInt16(CurrentSubDevice.N_Thread)) / 1.6);
+
+            TextSize = BarSize / 2;
+
+            if (TextSize > 14)
+                TextSize = 14;
+
+            // Poistion de départ
+            BarPosition = 0;
+
+            for (int i = 0; i < Convert.ToInt16(CurrentSubDevice.N_Thread); i++)
+            {
+                WinformComponents.ChartBar chart = new WinformComponents.ChartBar();
+                Label label = new Label();
+
+                chart.Activated = true;
+                chart.BarBackColor = System.Drawing.Color.LightGray;
+                chart.BarFillColor = System.Drawing.Color.LightSeaGreen;
+                chart.BarFillSize = 25;
+                chart.Location = new System.Drawing.Point(10, BarPosition);
+                chart.Size = new System.Drawing.Size(190, BarSize);
+                chart.Style = WinformComponents.ChartBar.STYLE.Horizontal;
+                chart.TextColor = System.Drawing.Color.White;
+                chart.TextVisible = true;
+                chart.Name = typeof(WinformComponents.ChartBar).ToString();
+
+                label.Font = new System.Drawing.Font("Microsoft Sans Serif", TextSize, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                label.Location = new System.Drawing.Point(chart.Width + 15, BarPosition);
+                label.Size = new System.Drawing.Size(100, BarSize);
+                label.Text = "TH " + i;
+                label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+
+                // Insertion dans le panneau
+                this.pnl_Threads.Controls.Add(chart);
+                this.pnl_Threads.Controls.Add(label);
+
+                //Mis à jour de la position
+                BarPosition = BarPosition + Step;
+            }
+        }
+
+        private void Clear()
+        {
+            this.pnl_Threads.Controls.Clear();
+        }
+    }
+}
