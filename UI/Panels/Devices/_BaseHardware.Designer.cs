1d4f3bb043d3237ab2565d9a51dd71dd319d63cd
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-12 20:48:59 +0100

    rename control

--- UI/Panels/Devices/_BaseHardware.Designer.cs
+++ UI/Panels/Devices/_BaseHardware.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.pic_Divisor_001 = new System.Windows.Forms.PictureBox();
-            this._BasePcDevicePerformanceControl1 = new DiscoveryLight.UI.DeviceControls.DevicePerformanceControls._BasePcDevicePerformanceControl();
-            this._BasePcDeviceDataControl1 = new DiscoveryLight.UI.DeviceControls.DeviceDataControls._BasePcDeviceDataControl();
+            this.BasePcDevicePerformanceControl = new DiscoveryLight.UI.DeviceControls.DevicePerformanceControls._BasePcDevicePerformanceControl();
+            this.BasePcDeviceDataControl = new DiscoveryLight.UI.DeviceControls.DeviceDataControls._BasePcDeviceDataControl();
             ((System.ComponentModel.ISupportInitialize)(this.pic_Divisor_001)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,34 +44,34 @@
             this.pic_Divisor_001.TabIndex = 22;
             this.pic_Divisor_001.TabStop = false;
             // 
-            // _BasePcDevicePerformanceControl1
+            // BasePcDevicePerformanceControl
             // 
-            this._BasePcDevicePerformanceControl1.CurrentPerformance = null;
-            this._BasePcDevicePerformanceControl1.CurrentSubDevice = 0;
-            this._BasePcDevicePerformanceControl1.Location = new System.Drawing.Point(476, 0);
-            this._BasePcDevicePerformanceControl1.Name = "_BasePcDevicePerformanceControl1";
-            this._BasePcDevicePerformanceControl1.Period = System.TimeSpan.Parse("00:00:00");
-            this._BasePcDevicePerformanceControl1.Size = new System.Drawing.Size(127, 360);
-            this._BasePcDevicePerformanceControl1.TabIndex = 24;
-            this._BasePcDevicePerformanceControl1.TokenSource = null;
+            this.BasePcDevicePerformanceControl.CurrentPerformance = null;
+            this.BasePcDevicePerformanceControl.CurrentSubDevice = 0;
+            this.BasePcDevicePerformanceControl.Location = new System.Drawing.Point(476, 0);
+            this.BasePcDevicePerformanceControl.Name = "BasePcDevicePerformanceControl";
+            this.BasePcDevicePerformanceControl.Period = System.TimeSpan.Parse("00:00:00");
+            this.BasePcDevicePerformanceControl.Size = new System.Drawing.Size(127, 360);
+            this.BasePcDevicePerformanceControl.TabIndex = 24;
+            this.BasePcDevicePerformanceControl.TokenSource = null;
             // 
-            // _BasePcDeviceDataControl1
+            // BasePcDeviceDataControl
             // 
-            this._BasePcDeviceDataControl1.CurrentDevice = null;
-            this._BasePcDeviceDataControl1.CurrentSubDevice = null;
-            this._BasePcDeviceDataControl1.DeviceName = null;
-            this._BasePcDeviceDataControl1.DeviceType = null;
-            this._BasePcDeviceDataControl1.Location = new System.Drawing.Point(19, 0);
-            this._BasePcDeviceDataControl1.Name = "_BasePcDeviceDataControl1";
-            this._BasePcDeviceDataControl1.Size = new System.Drawing.Size(416, 360);
-            this._BasePcDeviceDataControl1.TabIndex = 23;
+            this.BasePcDeviceDataControl.CurrentDevice = null;
+            this.BasePcDeviceDataControl.CurrentSubDevice = null;
+            this.BasePcDeviceDataControl.DeviceName = null;
+            this.BasePcDeviceDataControl.DeviceType = null;
+            this.BasePcDeviceDataControl.Location = new System.Drawing.Point(19, 0);
+            this.BasePcDeviceDataControl.Name = "BasePcDeviceDataControl";
+            this.BasePcDeviceDataControl.Size = new System.Drawing.Size(416, 360);
+            this.BasePcDeviceDataControl.TabIndex = 23;
             // 
             // _BaseHardware
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this._BasePcDevicePerformanceControl1);
-            this.Controls.Add(this._BasePcDeviceDataControl1);
+            this.Controls.Add(this.BasePcDevicePerformanceControl);
+            this.Controls.Add(this.BasePcDeviceDataControl);
             this.Controls.Add(this.pic_Divisor_001);
             this.Name = "_BaseHardware";
             this.Size = new System.Drawing.Size(630, 360);
@@ -82,7 +82,7 @@
 
         #endregion
         private System.Windows.Forms.PictureBox pic_Divisor_001;
-        private DeviceControls.DeviceDataControls._BasePcDeviceDataControl _BasePcDeviceDataControl1;
-        private DeviceControls.DevicePerformanceControls._BasePcDevicePerformanceControl _BasePcDevicePerformanceControl1;
+        private DeviceControls.DeviceDataControls._BasePcDeviceDataControl BasePcDeviceDataControl;
+        private DeviceControls.DevicePerformanceControls._BasePcDevicePerformanceControl BasePcDevicePerformanceControl;
     }
 }
