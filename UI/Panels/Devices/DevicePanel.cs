c3388b0b4f7b92e828f5ea70a500a682ac7f931b
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-15 03:27:06 +0100

    use auto clear current loaded performance

--- UI/Panels/Devices/DevicePanel.cs
+++ UI/Panels/Devices/DevicePanel.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DiscoveryLight.Core.Device.Performance;
+using DiscoveryLight.UI.DeviceControls.DevicePerformanceControls;
 
 namespace DiscoveryLight.UI.Panels.Devices
 {
     public class DevicePanel: UserControl
     {
+        public List<DevicePerformance> LoadedPerformance;
+        
 
-        public DevicePanel(): base()
+        public void StopLoadedPerformance()
         {
+            foreach (Control c in this.Controls)
+            {
+                var res = c.GetType().BaseType.FullName;
+                var res2 = typeof(DevicePerformanceControl).ToString();
+                if (c.GetType().BaseType.FullName == typeof(DevicePerformanceControl).ToString())
+                {
+                    var t = (DevicePerformanceControl)c;
+                    t.StopPerformance();
+                }
+            }
+        }
 
+        public DevicePanel()
+        {
+            LoadedPerformance = new List<DevicePerformance>();
+            
         }
     }
 }
