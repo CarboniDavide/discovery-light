101b4aefc7bdd3a074eedf01738649533a107917
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-15 14:03:15 +0100

    use panel container to manage all device panel

---
+++ UI/Panels/Devices/_PanelContainer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiscoveryLight.UI.Panels.Devices
+{
+    public partial class _PanelContainer : UserControl
+    {
+
+        private DevicePanel currentPanel;
+        public DevicePanel CurrentPanel { get => currentPanel; set => currentPanel = value; }
+
+        public _PanelContainer()
+        {
+            InitializeComponent();
+        }
+
+        
+        public void LoadPanel(string name)
+        {
+            this.RemovePanel();
+            this.CurrentPanel = PanelCollection.PanelFactory(name);
+            this.Controls.Add(CurrentPanel);
+        }
+
+        public void RemovePanel()
+        {
+            if (this.Controls.Count == 0) return;
+            this.CurrentPanel.StopLoadedPerformance();
+            this.Controls.Clear();
+        }
+    }
+}
