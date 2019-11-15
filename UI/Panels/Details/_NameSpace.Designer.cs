2b1ef73a76ffd848f2b44d9c609a3cdb45246ba0
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-15 19:17:18 +0100

    implement panel to list all namespace in wmi class

---
+++ UI/Panels/Details/_NameSpace.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace DiscoveryLight.UI.Panels.Details
+{
+    partial class _NameSpace
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lst_NameSpace = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lst_NameSpace
+            // 
+            this.lst_NameSpace.BackColor = System.Drawing.SystemColors.Control;
+            this.lst_NameSpace.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lst_NameSpace.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lst_NameSpace.FormattingEnabled = true;
+            this.lst_NameSpace.Location = new System.Drawing.Point(0, 0);
+            this.lst_NameSpace.Name = "lst_NameSpace";
+            this.lst_NameSpace.Size = new System.Drawing.Size(630, 360);
+            this.lst_NameSpace.TabIndex = 0;
+            // 
+            // _NameSpace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lst_NameSpace);
+            this.Name = "_NameSpace";
+            this.Size = new System.Drawing.Size(630, 360);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lst_NameSpace;
+    }
+}
---
+++ UI/Panels/Details/_NameSpace.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using DiscoveryLight.Logging;
+
+namespace DiscoveryLight.UI.Panels.Details
+{
+    public partial class _NameSpace : BasePanel
+    {
+        public _NameSpace()
+        {
+            InitializeComponent();
+            InitAndRun(this.lst_NameSpace);
+        }
+
+        public override IEnumerable<String> Get()
+        {
+            base.Get();
+            ManagementClass nsClass;
+
+            try
+            {
+                // __namespace WMI class.
+                nsClass = new ManagementClass(new ManagementScope("root"), new ManagementPath("__namespace"), null);
+            }
+            catch (ManagementException e)
+            {
+                LogHelper.Log(LogTarget.File, e.ToString());
+                nsClass = null;
+            }
+
+            if (nsClass == null) yield return null;
+
+            foreach (ManagementObject ns in nsClass.GetInstances())
+                    yield return ns["Name"].ToString().ToUpper();
+            
+        }
+    }
+}
