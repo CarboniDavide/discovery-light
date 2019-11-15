0d42cddb225cee4cac80d7404593a34e53a234a6
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-15 19:15:03 +0100

    implements panel to have all details for a selected wmi class

---
+++ UI/Panels/Details/_Classes.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace DiscoveryLight.UI.Panels.Details
+{
+    partial class _Classes
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lst_Classe = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lst_Classe
+            // 
+            this.lst_Classe.BackColor = System.Drawing.SystemColors.Control;
+            this.lst_Classe.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lst_Classe.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lst_Classe.FormattingEnabled = true;
+            this.lst_Classe.Location = new System.Drawing.Point(0, 0);
+            this.lst_Classe.Name = "lst_Classe";
+            this.lst_Classe.Size = new System.Drawing.Size(630, 360);
+            this.lst_Classe.TabIndex = 0;
+            // 
+            // _Classes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lst_Classe);
+            this.Name = "_Classes";
+            this.Size = new System.Drawing.Size(630, 360);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lst_Classe;
+    }
+}
---
+++ UI/Panels/Details/_Classes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using DiscoveryLight.Logging;
+
+namespace DiscoveryLight.UI.Panels.Details
+{
+    public partial class _Classes : BasePanel
+    {
+        public _Classes()
+        {
+            InitializeComponent();
+            InitAndRun(this.lst_Classe);
+        }
+
+        public override IEnumerable<String> Get()
+        {
+            base.Get();
+
+            ManagementObjectSearcher nsClass;
+
+            try
+            {
+                // __namespace WMI class.
+                nsClass = new ManagementObjectSearcher(new ManagementScope("root\\CIMV2"), new WqlObjectQuery("select * from meta_class"), null);
+            }
+            catch (ManagementException e)
+            {
+                LogHelper.Log(LogTarget.File, e.ToString());
+                nsClass = null;
+            }
+
+            if (nsClass == null) yield return null;
+
+            foreach (ManagementClass wmiClass in nsClass.Get())
+                foreach (QualifierData qd in wmiClass.Qualifiers)
+                    // Si la classe est dynamique, ajoute les valeurs dans la liste.
+                    if (qd.Name.Equals("dynamic") || qd.Name.Equals("static"))
+                        yield return wmiClass["__CLASS"].ToString();
+        }
+    }
+}
