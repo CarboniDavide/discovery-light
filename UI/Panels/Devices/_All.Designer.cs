c7003cb7df36367e2b5ea445f704b79a0fffde74
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-15 03:49:10 +0100

    prepare details panel

--- UI/Panels/Devices/_All.Designer.cs
+++ UI/Panels/Devices/_All.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Size = new System.Drawing.Size(630, 360);
+            this.ResumeLayout(false);
         }
 
         #endregion
