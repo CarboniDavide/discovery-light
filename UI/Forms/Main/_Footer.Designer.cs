64b497d1cbc4cdb4426e87925d7ab05cd61ecfc8
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-15 03:28:55 +0100

    implements footer bar

--- UI/Forms/Main/_Footer.Designer.cs
+++ UI/Forms/Main/_Footer.Designer.cs
@@ -28,18 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbl_DeviceName = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lbl_DeviceName
+            // 
+            this.lbl_DeviceName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_DeviceName.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.lbl_DeviceName.ForeColor = System.Drawing.Color.Salmon;
+            this.lbl_DeviceName.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbl_DeviceName.Location = new System.Drawing.Point(227, 0);
+            this.lbl_DeviceName.Name = "lbl_DeviceName";
+            this.lbl_DeviceName.Size = new System.Drawing.Size(333, 40);
+            this.lbl_DeviceName.TabIndex = 5;
+            this.lbl_DeviceName.Text = "N/A";
+            this.lbl_DeviceName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // _Footer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Controls.Add(this.lbl_DeviceName);
             this.Name = "_Footer";
+            this.Size = new System.Drawing.Size(560, 40);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        public System.Windows.Forms.Label lbl_DeviceName;
     }
 }
