73012d692dc596e03d001efc8d61b9bb481c728b
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-21 00:46:34 +0100

    create and implements slider animation

---
+++ UI/Panels/Slider/_Slider.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace DiscoveryLight.UI.Panels.Slider
+{
+    partial class _Slider
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MoveOBject = new WinformComponents.MoveOBject();
+            this.SuspendLayout();
+            // 
+            // MoveOBject
+            // 
+            this.MoveOBject.Interval = 10;
+            this.MoveOBject.IsOperating = false;
+            this.MoveOBject.Object = this;
+            this.MoveOBject.Type = WinformComponents.MoveOBject.TYPE.Vertical;
+            // 
+            // _Slider
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.DoubleBuffered = true;
+            this.Name = "_Slider";
+            this.Size = new System.Drawing.Size(630, 360);
+            this.Load += new System.EventHandler(this._Slider_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WinformComponents.MoveOBject MoveOBject;
+    }
+}
---
+++ UI/Panels/Slider/_Slider.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiscoveryLight.UI.Panels.Devices;
+
+namespace DiscoveryLight.UI.Panels.Slider
+{
+    public partial class _Slider : UserControl
+    {
+        public _Slider()
+        {
+            InitializeComponent();
+        }
+
+        DevicePanel CurrentPanel;
+        DevicePanel NewPanel;
+        Action MoveAction;
+
+        public void AddSlide(DevicePanel panel)
+        {
+            NewPanel = panel;
+            if (this.Controls.Count == 0)
+            {
+                CurrentPanel = panel;
+                this.Controls.Add(CurrentPanel);
+                return;
+            }
+
+            CurrentPanel = this.Controls[0] as DevicePanel;
+            if (!PanelSetPosition()) return;
+            this.Controls.Add(NewPanel);
+            Start();
+        }
+
+        public void RemoveSlide()
+        {
+            this.Controls.Remove(CurrentPanel);
+            CurrentPanel.Dispose();
+        }
+
+        public Boolean PanelSetPosition()
+        {
+            if (this.NewPanel.PanelIndex == this.CurrentPanel.PanelIndex) return false;
+
+            if (this.NewPanel.PanelIndex > this.CurrentPanel.PanelIndex)
+            {
+                MoveAction = MoveDown;
+                this.Height = NewPanel.Height *2;
+                this.Location = new Point(this.Location.X, -NewPanel.Height);
+                this.Controls[0].Location = new Point(this.Location.X, NewPanel.Height);
+                NewPanel.Location = new Point(CurrentPanel.Location.X, 0);
+            }
+            else
+            {
+                MoveAction = MoveUp;
+                this.Height = NewPanel.Height * 2;
+                this.Location = new Point(this.Location.X, 0);
+                NewPanel.Location = new Point(CurrentPanel.Location.X, CurrentPanel.Height);
+            }
+
+            return true;
+        }
+
+        public void Start()
+        {
+            MoveAction.Invoke();
+        }
+
+        public void MoveUp()
+        {
+            MoveOBject.MoveObject(-NewPanel.Height);
+        }
+
+        public void MoveDown()
+        {
+            MoveOBject.MoveObject(0);
+        }
+
+        private void OnFinish(object sender, EventArgs e)
+        {
+            RemoveSlide();
+        }
+
+        private void _Slider_Load(object sender, EventArgs e)
+        {
+            this.MoveOBject.OnFinishMoving += OnFinish;
+        }
+    }
+}
