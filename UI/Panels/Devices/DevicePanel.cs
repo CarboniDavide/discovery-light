3f2b1c85b58e2c0539c20845c040867f4684d86c
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-11-21 00:49:41 +0100

    add panel index and start/stop automatically performance

--- UI/Panels/Devices/DevicePanel.cs
+++ UI/Panels/Devices/DevicePanel.cs
@@ -14,28 +14,51 @@ namespace DiscoveryLight.UI.Panels.Devices
     public abstract class AbstractDevicePanel: UserControl
     {
         public abstract void StopLoadedTask();
+        public abstract void StartAllTask();
 
     }
     public class DevicePanel: AbstractDevicePanel
     {
-        public List<DevicePerformance> LoadedPerformance;
+        private int panelIndex;
+        private Control parentContainer;
 
-        public override void StopLoadedTask()
-        {
+        public int PanelIndex { get => panelIndex; set => panelIndex = value; }
+        public Control ParentContainer { get => parentContainer; set => parentContainer = value; }
+
+        public override void StopLoadedTask(){
             foreach (Control c in this.Controls)
-            {
-                if (c.GetType().BaseType.FullName == typeof(DevicePerformanceControl).ToString())
-                {
+                if (c.GetType().BaseType.FullName == typeof(DevicePerformanceControl).ToString()){
                     var t = (DevicePerformanceControl)c;
                     t.StopPerformance();
                 }
-            }
         }
 
-        public DevicePanel()
+        public override void StartAllTask()
         {
-            LoadedPerformance = new List<DevicePerformance>();
-            
+            foreach (Control c in this.Controls)
+                if (c.GetType().BaseType.FullName == typeof(DevicePerformanceControl).ToString())
+                {
+                    var t = (DevicePerformanceControl)c;
+                    t.RunPerformance();
+                }
+        }
+
+        private void onCreate(object sender, EventArgs e)
+        {
+            StartAllTask();
+        }
+
+        private void onRemove(object sender, EventArgs e)
+        {
+            StopLoadedTask();
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public DevicePanel() {
+            //get parent container
+            this.Disposed += onRemove;
+            this.Load += onCreate;
         }
     }
 }
