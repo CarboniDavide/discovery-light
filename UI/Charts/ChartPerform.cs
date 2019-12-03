e6b7ca3c34ef8626481db0469516ae32dbb88b57
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-12-03 19:19:49 +0100

    implements chart perform values

---
+++ UI/Charts/ChartPerform.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinformComponents;
+
+namespace DiscoveryLight.UI.Charts
+{
+    public static class ChartPerform
+    {
+        /// <summary>
+        /// Return passed "Value", null elsewhere
+        /// </summary>
+        /// <param name="Value"></param>
+        /// <param name="Default"></param>
+        /// <returns></returns>
+        public static int? FillOrDefault(String Value)
+        {
+            if (Value == null) return null;
+            return Convert.ToInt32(Value);
+        }
+
+        /// <summary>
+        /// Return passed "Value", null elsewhere
+        /// </summary>
+        /// <param name="Value"></param>
+        /// <param name="Default"></param>
+        /// <returns></returns>
+        public static int? FillOrDefault(int? Value)
+        {
+            if (Value == null) return null;
+            return Value;
+        }
+
+        /// <summary>
+        /// Return lambda value for param "Value",null elsewhere
+        /// </summary>
+        /// <param name="Value"></param>
+        /// <param name="Default"></param>
+        /// <returns></returns>
+        public static int? FillOrDefault(Func<String, int?> Lambda, String Value)
+        {
+            if (Value == null) return null;
+            return Convert.ToInt32(Lambda.Invoke(Value));
+        }
+
+        /// <summary>
+        /// Return lambda value for param "Value", null elsewhere
+        /// </summary>
+        /// <param name="Value"></param>
+        /// <param name="Default"></param>
+        /// <returns></returns>
+        public static int? FillOrDefault(Func<int?, int?> Lambda, int? Value)
+        {
+            if (Value == null) return null;
+            return Lambda.Invoke(Value);
+        }
+
+    }
+}
