70a2c0c6b69a5908ad393c3e7b53e56a567684ce
CarboniDavide <carbo.davide@gmail.com>
2019-12-22 20:37:36 +0100

    use implicite string to drive index

--- Core/Device/DevicePerformance.cs
+++ Core/Device/DevicePerformance.cs
@@ -297,9 +297,10 @@ namespace DiscoveryLight.Core.Device.Performance
             var mj = new WprManagementObjectSearcher("Win32_DiskDrive").First(x => x.GetProperty("Caption").AsString().Equals(DevicetoToRelate)) ?? new WprManagementObject();
             foreach (WprManagementObject mjt in new WprManagementObjectSearcher("Win32_PerfRawData_PerfDisk_PhysicalDisk").All() ?? new List<WprManagementObject>())
             {
-                String currentDrive = mjt.GetProperty("Name").AsString();
-                var s = mj.GetProperty("Index").AsString();
-                if (currentDrive != null && currentDrive.Substring(0, 1).Equals(mj.GetProperty("Index").AsString()))
+                string currentDrive = mjt.GetProperty("Name").AsString();
+                string driveIndex = mj.GetProperty("Index").AsString();
+
+                if (currentDrive != null && currentDrive.Substring(0, 1).Equals(driveIndex))
                     DeviceRelated = (currentDrive.Substring(2, 1) + ":").ToString();
             }
 
